d71fcc779869ae063a10c1270d9c10511ce618f4
user <cleosignori@gmail.com>
2024-01-17 15:44:10 -0300

    refactor: add MAX Method

--- MicrosoftSQLServer/datepart.sql
+++ MicrosoftSQLServer/datepart.sql
@@ -1,10 +1,9 @@
 -- DatePart
 
 -- Selecionar o Mês que mais foi gasto com o pagamento de funcionários.
-SELECT TOP 1 SUM(Rate) AS PaymentTotal, DATEPART(MONTH, RateChangeDate) AS MonthPayment
+SELECT MAX(Rate) AS PaymentTotal, DATEPART(MONTH, RateChangeDate) AS MonthPayment
 FROM HumanResources.EmployeePayHistory
 GROUP BY DATEPART(MONTH, RateChangeDate)
-ORDER BY PaymentTotal DESC
 
 /*
 	SELECT AVG(TotalDue) AS Media, DATEPART(year, OrderDate) AS MES
