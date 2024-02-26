8ff707d80116623b29334e5e04e29e2451a3506a
Ruan Pablo Signori Preto <cleosignori@gmail.com>
2024-02-26 07:39:48 -0300

    group by

---
+++ postgres/agrupamento.sql
@@ -0,0 +1,15 @@
+-- Quando precisar usar uma função de agregação, usar o GROUP BY, se não, pode usar o DISTINCT
+
+-- Retorna apenas os valores que são diferentes
+SELECT DISTINCT
+	nome,
+	sobrenome
+FROM funcionarios
+ORDER BY nome;
+
+SELECT DISTINCT
+	nome,
+	sobrenome.
+    COUNT(*)
+FROM funcionarios
+ORDER BY nome;
